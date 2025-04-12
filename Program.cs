44470009ebb666590d68239b9333e080cc79ecd0
gustavo08_fonseca@outlook.com <gustavo08_fonseca@outlook.com>
2025-04-12 13:45:19 -0300

    Changing DBContexto for EF Migrations work correctly

--- Program.cs
+++ Program.cs
@@ -14,13 +14,14 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var app = builder.Build();
+
+
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     db.Database.Migrate();
 }
-
-var app = builder.Build();
 
 // Configurar o Swagger
 if (app.Environment.IsDevelopment())
